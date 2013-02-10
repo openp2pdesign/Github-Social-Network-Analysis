7828b8de2ef5c6326f611fd5b21652d434a37f9e
Massimo Menichinelli <info@openp2pdesign.org>
2013-02-10 20:33:14 +0100

    Adding the ego-network script

---
+++ ego-network.py
@@ -0,0 +1,29 @@
+from github import Github
+import networkx as nx
+
+print "Social Network Analisys of your GitHub network"
+print ""
+username = raw_input("Enter your username: ")
+password = raw_input("Enter your password: ")
+print ""
+g = Github( username, password )
+
+graph = nx.DiGraph()
+
+print "Looking for your followers..."
+for f in g.get_user().get_followers():
+    print " -", f.name
+    graph.add_edge(f.name,username)
+    
+print "-----"
+
+print "Looking for the ones you are following..."
+for f in g.get_user().get_following():
+    print " -", f.name
+    graph.add_edge(username,f.name)
+    
+print "-----"
+
+print "Saving the network..."
+nx.write_gexf(graph, username+"_ego-network.gexf")
+print "Done."
