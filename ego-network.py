2c6debbd080875d001de0585c77bdb8e87d9a97d
Massimo Menichinelli <info@openp2pdesign.org>
2013-02-10 22:48:13 +0100

    Improving the ego network script

--- ego-network.py
+++ ego-network.py
@@ -1,34 +1,47 @@
+#
 # Author: Massimo Menichinelli
 # Homepage: http://www.openp2pdesign.org
 # License: GPL v.3
 #
-# Instructions: install pyGithub with pip install PyGithub
+# Requisite: install pyGithub with pip install PyGithub
 #
-
 
 from github import Github
 import networkx as nx
+import getpass
 
 print "Social Network Analisys of your GitHub network"
 print ""
 username = raw_input("Enter your username: ")
-password = raw_input("Enter your password: ")
+password = getpass.getpass("Enter yor password: ") 
 print ""
 g = Github( username, password )
 
 graph = nx.DiGraph()
 
+graph.add_node(username,label=g.get_user().name)
+
 print "Looking for your followers..."
 for f in g.get_user().get_followers():
-    print " -", f.name
-    graph.add_edge(f.name,username)
-    
+    print " -", f.login, " / ", f.name
+    if f.name == None:
+        graph.add_node(f.login,label=f.login)
+        graph.add_edge(f.login,username)
+    else:
+        graph.add_node(f.login,label=f.name)
+        graph.add_edge(f.login,username)
+        
 print "-----"
 
-print "Looking for the ones you are following..."
+print "Looking for the users you are following..."
 for f in g.get_user().get_following():
-    print " -", f.name
-    graph.add_edge(username,f.name)
+    print " -", f.login, " / ", f.name
+    if f.name == None:
+        graph.add_node(f.login,label=f.login)
+        graph.add_edge(username,f.login)
+    else:
+        graph.add_node(f.login,label=f.name)
+        graph.add_edge(username,f.login)
     
 print "-----"
 
