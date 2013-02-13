d796b9b270283819ffc78fd601e66d7423032dc9
Massimo Menichinelli <info@openp2pdesign.org>
2013-02-13 17:37:11 +0100

    Updating the organization repository analysis

--- organization_repository_analysis.py
+++ organization_repository_analysis.py
@@ -1,5 +1,5 @@
 #
-# Social network analysis of an Organization repository in GitHub
+# Basic analysis of an Organization repository in GitHub
 #
 # Author: Massimo Menichinelli
 # Homepage: http://www.openp2pdesign.org
@@ -49,33 +49,49 @@ print ""
 
 repo_to_mine = raw_input("Enter the name of the repository you want to mine: ")
 
-
 a = org.get_repo(repo_to_mine)
 print "-----"
 print "DESCRIPTION:",a.description
 print "-----"
+print "OWNER:",a.owner.login
+print "-----"
+print "WATCHERS:",a.watchers
+for i in a.get_stargazers():
+    if i != None:
+        print "-",i.login
+print "-----"
 print "COLLABORATORS"
 for i in a.get_collaborators():
-    print "-",i.login
+    if i != None:
+        print "-",i.login
 print "-----"
 print "HAS ISSUES=",a.has_issues
 if a.has_issues == True:
     print "-----"
     print "ISSUES"
     for i in a.get_issues():
-        print "- Created by", i.user.login
+        if i.user != None:
+            print "- Created by", i.user.login
         print "--",i.title
+        if i.assignee != None:
+            print "-- Assigned to",i.assignee.login
         print "--",i.comments,"comments"
         for f in i.get_comments():
-            print "--- With a comment by",f.user.login
+            if f.user != None:
+                print "--- With a comment by",f.user.login
         print ""        
 print "-----"
 print "CONTRIBUTORS"
 for i in a.get_contributors():
     print "-", i.login
 print "-----"
+print "COMMITS"
+for i in  a.get_commits():
+    print "-",i.sha
+    if i.committer != None:
+        print "-- by",i.committer.login
+print "-----"
 print "FORKS"
 for i in a.get_forks():
     print i.name
 print "-----"
-
