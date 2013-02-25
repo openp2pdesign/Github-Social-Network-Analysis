9d0eb997cb1cd462df0fa70083fc022ad9cc7012
Massimo Menichinelli <info@openp2pdesign.org>
2013-02-25 13:54:20 +0200

    Adding an Ego-network of organizations, 1 and 2 degrees

---
+++ organization_ego-network-2levels.py
@@ -0,0 +1,89 @@
+#
+# Ego-network analysis of followers in a GitHub organization, Degree 2 
+# Fast version (only account name)
+#
+# Author: Massimo Menichinelli
+# Homepage: http://www.openp2pdesign.org
+# License: GPL v.3
+#
+# Requisite: 
+# install pyGithub with pip install PyGithub
+# install NetworkX with pip install networkx
+#
+# PyGitHub documentation can be found here: 
+# https://github.com/jacquev6/PyGithub
+#
+
+from github import Github
+import networkx as nx
+import getpass
+
+print "Organization Ego-network analysis"
+print ""
+userlogin = raw_input("Login: Enter your username: ")
+password = getpass.getpass("Login: Enter yor password: ")
+username = raw_input("Enter the username you want to analyse: ")
+print ""
+g = Github( userlogin, password )
+
+print "ORGANIZATIONS:"
+for i in g.get_user(username).get_orgs():
+    print "-", i.login
+print ""
+
+org_to_mine = raw_input("Enter the name of the Organization you want to analyse: ")
+print ""
+org = g.get_organization(org_to_mine)
+
+graph = nx.DiGraph()
+
+for i in org.get_members():
+    print "Member:",i.login
+    graph.add_node(i.login,label=i.login,member="Yes")
+
+
+for j in org.get_members():
+    print ""
+    print "-----"
+    print "Looking for the followers of", j.login,"..."
+    for f in g.get_user(j.login).get_followers():
+        print " -", f.login
+        graph.add_node(f.login,label=f.login, member="No")
+        graph.add_edge(f.login,j.login)
+        print " And his/her followers:"
+        for i in f.get_followers():
+            print " --", i.login
+            graph.add_node(i.login,label=i.login, member="No")
+            graph.add_edge(i.login,f.login)
+        print " And the users she/he's following:"
+        for i in f.get_following():
+            print " --", i.login
+            graph.add_node(i.login,label=i.login, member="No")
+            graph.add_edge(f.login,i.login)
+                 
+    print "-----"
+    
+    print "Looking for the users that",j.login,"is following ..."
+    for f in g.get_user(j.login).get_following():
+        print " -", f.login
+        graph.add_node(f.login,label=f.login,member="No")
+        graph.add_edge(j.login,f.login)
+        print " And his/her followers:"
+        for i in f.get_followers():
+            print " --", i.login
+            graph.add_node(i.login,label=i.login,member="No")
+            graph.add_edge(i.login,f.login)
+        print " And the users she/he's following:"
+        for i in f.get_following():
+            print " --", i.login
+            graph.add_node(i.login,label=i.login,member="No")
+            graph.add_edge(f.login,i.login)
+               
+    print "-----"
+
+for i in org.get_members():
+    graph.node[i.login]["member"]="Yes"
+
+print "Saving the network..."
+nx.write_gexf(graph,username+"_"+org_to_mine+"_ego-network_2_levels.gexf")
+print "Done."
